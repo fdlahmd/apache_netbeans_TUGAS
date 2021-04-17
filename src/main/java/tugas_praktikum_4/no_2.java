a463e674b3616099d527f0e5d34cf3074e7b3d2a
x <x@DESKTOP-VJQ2VHU>
2021-04-17 11:26:41 +0700

    Tugas

---
+++ src/main/java/tugas_praktikum_4/no_2.java
@@ -0,0 +1,28 @@
+package tugas_praktikum_4;
+public class no_2 {
+    public static void main(String[] args) {
+        int[] data = {25,7,9,13,3};
+        for (int i=0; i<data.length; i++){
+            System.out.print(data[i]+" ");
+        }
+        int k=1;
+        while (k<data.length){
+            int j = data.length-1;
+            while (j>=1){
+                if (data[j-1]>data[j]){
+                    int temp = data[j];
+                    data[j] = data[j-1];
+                    data[j-1] = temp;
+                }
+                j = j-1;
+            }
+            k = k+1;
+        }
+        System.out.println("\n---- Bubble Sort ----");
+        for (int i=0; i<data.length; i++){
+            System.out.print(data[i]+" ");
+        }
+        System.out.println("\n");
+        System.out.println("Fadhil Ahmad Hidayat");
+    }
+}
